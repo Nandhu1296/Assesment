62af03403a89282ed39199e03fc33d1dd4abc4ec
nandh <nandh@LAPTOP-1ANN05DQ>
2020-05-21 13:18:49 +0530

    2nd Commit after step 2 (adding unknown amount of numbers).

--- helloworld_nandha/src/main/java/helloworld_nandha/calcMain.java
+++ helloworld_nandha/src/main/java/helloworld_nandha/calcMain.java
@@ -2,11 +2,22 @@ package helloworld_nandha;
 
 public class calcMain {
 	
-	public String play(String c1, String c2)
+	public String play(String numbers)
 	{
-		int n1=Integer.parseInt(c1);
-		int n2=Integer.parseInt(c2);
-		int resNum=n1+n2;
-		return String.valueOf(resNum);
+		int counts = 0;
+		int resNum=0;
+		if(numbers.contains(" ") || numbers.isEmpty())
+		{
+			return String.valueOf(resNum);		
+		}
+		else {
+			String[] numberSplit = numbers.split(",");
+			for (int i = 0; i < numberSplit.length; i++) 
+			{
+				resNum=resNum+Integer.parseInt(numberSplit[i]);
+				counts=counts+1;
+			}
+			return String.valueOf(resNum);
+		}
 	}
 }
--- helloworld_nandha/src/test/java/helloworld_nandha/calcTest.java
+++ helloworld_nandha/src/test/java/helloworld_nandha/calcTest.java
@@ -1,6 +1,5 @@
 package helloworld_nandha;
 
-import java.awt.font.NumericShaper;
 import java.util.Scanner;
 
 import org.junit.*;
@@ -14,31 +13,11 @@ public class calcTest {
 	{
 		calcMain calcMainObj= new calcMain();
 		sc = new Scanner(System.in);
-		int counts = 0;
 		System.out.println("Enter the two numbers seperated by comma's \n");
 		String numbers= sc.nextLine();
 		
-		if(numbers.contains(" ") || numbers.isEmpty()) {
-			int newNum=0;
-			System.out.println("The result is \n"+ newNum);
-		}
-		else {
-			String[] numberSplit = numbers.split(",");
-			for (int i = 0; i < numberSplit.length; i++) 
-			{
-				counts=counts+1;
-			}
-			if(counts == 1) {
-				String newNum=numberSplit[0];
-				System.out.println("The result is \n"+ newNum);
-			}else {
-				String c1= numberSplit[0];
-				String c2= numberSplit[1];
-				String numberReturned= calcMainObj.play(c1,c2);
-				System.out.println("The result is \n"+ numberReturned);
-			}
-			
-		}
+		String numberReturned= calcMainObj.play(numbers);
+		System.out.println("The result is \n"+ numberReturned);
 		
 	}
 	
